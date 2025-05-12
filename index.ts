2e17eefe27d2126da24e1f35de8dd0e426dcc167
liviacapile <livia.antoniassi@estudante.ifms.edu.br>
2025-05-12 16:45:51 -0400

    Fiz o get e o post no backend dos esmaltes (Lívia)

--- index.ts
+++ index.ts
@@ -21,6 +21,7 @@ app.get('/maquiagem', async (request: FastifyRequest, reply: FastifyReply) => {
         const [dados, camposTabela] = resultado
         reply.status(200).send(dados)
     }
+    
     catch (erro: any) {
         if (erro.code === 'ECONNREFUSED') {
             console.log("ERRO: LIGUE O LARAGAO => Conexão Recusada")
@@ -56,6 +57,89 @@ app.post('/maquiagem', async (request: FastifyRequest, reply: FastifyReply) => {
             port: 3306
         })
         const resultado = await conn.query("INSERT INTO maquiagem (id,nome_produto,quantidade_produto,validade) VALUES (?,?,?,?)",[id,nome_produto,quantidade_produto,validade])
+        const [dados, camposTabela] = resultado
+        reply.status(200).send(dados)
+    }
+    catch (erro: any) {
+        switch (erro.code) {
+            case "ECONNREFUSED":
+                console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!");
+                reply.status(400).send({ mensagem: "ERRO: LIGUE O LARAGÃO!!! CABEÇA!" });
+                break;
+            case "ER_BAD_DB_ERROR":
+                console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO" });
+                break;
+            case "ER_ACCESS_DENIED_ERROR":
+                console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO" });
+                break;
+            case "ER_DUP_ENTRY":
+                console.log("ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA");
+                reply.status(400).send({ mensagem: "ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA" });
+                break;
+            default:
+                console.log(erro);
+                reply.status(400).send({ mensagem: "ERRO DESCONHECIDO OLHE O TERMINAL DO BACKEND" });
+                break;
+        }
+
+    }
+})
+
+
+
+
+app.get('/esmalte', async (request: FastifyRequest, reply: FastifyReply) => {
+
+    try {
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password: "",
+            database: 'banco1023a',
+            port: 3306
+        })
+        const resultado = await conn.query("SELECT * FROM esmalte")
+        const [dados, camposTabela] = resultado
+        reply.status(200).send(dados)
+    }
+    
+    catch (erro: any) {
+        if (erro.code === 'ECONNREFUSED') {
+            console.log("ERRO: LIGUE O LARAGAO => Conexão Recusada")
+            reply.status(400).send({ mensagem: "ERRO: LIGUE O LARAGAO => Conexão Recusada" })
+        } else if (erro.code === 'ER_BAD_DB_ERROR') {
+            console.log("ERRO: CRIE UM BANCO DE DADOS COM O NOME DEFINIDO NA CONEXÃO")
+            reply.status(400).send({ mensagem: "ERRO: CRIE UM BANCO DE DADOS COM O NOME DEFINIDO NA CONEXÃO" })
+        } else if (erro.code === 'ER_ACCESS_DENIED_ERROR') {
+            console.log("ERRO: CONFERIR O USUÁRIO E SENHA DEFINIDOS NA CONEXÃO")
+            reply.status(400).send({ mensagem: "ERRO: CONFERIR O USUÁRIO E SENHA DEFINIDOS NA CONEXÃO" })
+        } else if (erro.code === 'ER_NO_SUCH_TABLE') {
+            console.log("ERRO: Você deve criar a tabela com o mesmo nome da sua QUERY")
+            reply.status(400).send({ mensagem: "ERRO: Você deve criar a tabela com o mesmo nome da sua QUERY" })
+        } else if (erro.code === 'ER_PARSE_ERROR') {
+            console.log("ERRO: Você tem um erro de escrita em sua QUERY confira: VÍRGULAS, PARENTESES E NOME DE COLUNAS")
+            reply.status(400).send({ mensagem: "ERRO: Você tem um erro de escrita em sua QUERY confira: VÍRGULAS, PARENTESES E NOME DE COLUNAS" })
+        } else {
+            console.log(erro)
+            reply.status(400).send({ mensagem: "ERRO: NÃO IDENTIFICADO" })
+        }
+    }
+})
+
+app.post('/esmalte', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id_esmalte, nome_cor, marca, data_fabricacao } = request.body as any
+
+    try {
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password: "",
+            database: 'banco1023a',
+            port: 3306
+        })
+        const resultado = await conn.query("INSERT INTO esmalte (id_esmalte,nome_cor,marca,data_fabricacao) VALUES (?,?,?,?)",[id_esmalte,nome_cor,marca,data_fabricacao])
         const [dados, camposTabela] = resultado
         reply.status(200).send(dados)
     }
